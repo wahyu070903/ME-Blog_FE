149dee4c4b4e466ebc2826529f61b2f24c8fb37a
wahyu070903 <wahyudwiahmadi793@gmail.com>
2024-10-11 02:33:55 +0700

    change to make mobile design first

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="flex flex-row items-center">
+    <div class="flex flex-row items-center justify-center w-[150%]">
         <button class="hidden md:block px-1.5 py-10 rounded mr-8 bg-base-yellow" @click="goToPrev">
             <i class="bi bi-chevron-left text-[32px] text-white"></i>
         </button>
-        <Splide :options="options" aria-label="My Favorite Images" ref="splide">
-            <SplideSlide v-for="data in carousel_data" class="w-auto ">
-                <Card :img_src = data.img :post_date = data.post :post_title = data.title :post_desc = data.desc :read_time = data.rTime />
-            </SplideSlide>
-        </Splide>
+        <div class="w-full">
+            <Splide :options="options" aria-label="My Favorite Images" ref="splide">
+                <SplideSlide v-for="data in carousel_data" >
+                    <Card :img_src = data.img :post_date = data.post :post_title = data.title :post_desc = data.desc :read_time = data.rTime />
+                </SplideSlide>
+            </Splide>
+        </div>
         <button class="hidden md:block px-1.5 py-10 rounded ml-8 bg-base-yellow" @click="goToNext">
             <i class="bi bi-chevron-right text-[32px] text-white"></i>
         </button>
@@ -16,9 +18,10 @@
 
 <script>
     import { Splide, SplideSlide } from '@splidejs/vue-splide';
+    import { defineComponent } from 'vue';
     import Card from './Card.vue'
 
-    export default {
+    export default defineComponent({
         components: {
             Splide,
             SplideSlide,
@@ -35,8 +38,17 @@
                 rewind : false,
                 perPage : 3,
                 perMove : 1,
+                gap : '1px',
                 arrows: false,
                 pagination : false,
+                focus : 'center',
+                trimSpace  : false,
+                breakpoints : {
+                    640 : {
+                        perPage : 2,
+                        gap : '1rem'
+                    }
+                }
             };
 
             return {options}
@@ -49,17 +61,9 @@
                 this.$refs.splide.go('>');
             }
         }
-    };
+    });
 </script>
 
 <style>
     @import '@splidejs/splide/dist/css/themes/splide-default.min.css';
-    .splide{
-        max-width: 988px;
-        /* 3 card width + padding 32px*2 */
-    }
-    .splide__slide{
-        width: auto !important;
-        margin-right: 32px;
-    }
 </style>
